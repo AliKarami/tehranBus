67ef0d23431739d46b0c10fc769288183147705b
Ali Karami <Ali.karami.m@gmail.com>
2017-02-15 16:31:07 +0330

    request library added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let bsCodes = require('./data/busStopCodes.json');
 let bsNames = require('./data/busStopNames.json');
-
+//send get Request to: https://application2.irantracking.com/FrontEndETA/MOBIL/api/ETA/GetETAPlusTripText2?currentStopId=4331&identifier=1&sequence=1
 let _getRemainingTimesByBSCode = () => {
 
 };
