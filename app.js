a25b58507eb032a809bcc989de3ce15e625ba692
Ali Karami <Ali.karami.m@gmail.com>
2017-02-15 16:18:14 +0330

    functions implemented

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 let bsCodes = require('./data/busStopCodes.json');
 let bsNames = require('./data/busStopNames.json');
 
+let _getRemainingTimesByBSCode = () => {
+
+};
+
+let _getRemainingTimesByBSName = () => {
+
+};
+
+module.exports = {
+	getRemainingTimesByBSCode: _getRemainingTimesByBSCode,
+	getRemainingTimesByBSName: _getRemainingTimesByBSName
+};
